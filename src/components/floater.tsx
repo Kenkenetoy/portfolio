31fbd9e64f265451fae40890457f7d08cd9a406d
Kenkenetoy <kennethjoseaparece@gmail.com>
2025-03-05 06:26:14 +0800

    fixed some href errors for an unsused component

--- src/components/floater.tsx
+++ src/components/floater.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable prettier/prettier */
 import {
   IconBrandGithub,
   IconBrandFacebook,
-  // IconBrandInstagram,
+  IconBrandInstagram,
   IconBrandLeetcode,
 } from "@tabler/icons-react";
 
@@ -15,37 +16,34 @@ export function FloatingDockDemo() {
       icon: (
         <IconBrandFacebook className="w-full h-full text-neutral-500 dark:text-neutral-300" />
       ),
-      href: siteConfig.links.facebook,
+      href: siteConfig.socials.facebook.link,
     },
-    // {
-    //   title: "Instagram",
-    //   icon: (
-    //     <IconBrandInstagram className="w-full h-full text-neutral-500 dark:text-neutral-300" />
-    //   ),
-    //   href: siteConfig.links.instagram,
-    // },
+    {
+      title: "Instagram",
+      icon: (
+        <IconBrandInstagram className="w-full h-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: siteConfig.socials.instagram.link,
+    },
     {
       title: "LeetCode",
       icon: (
         <IconBrandLeetcode className="w-full h-full text-neutral-500 dark:text-neutral-300" />
       ),
-      href: siteConfig.links.leetcode,
+      href: siteConfig.socials.leetcode.link,
     },
     {
       title: "GitHub",
       icon: (
         <IconBrandGithub className="w-full h-full text-neutral-500 dark:text-neutral-300" />
       ),
-      href: siteConfig.links.github,
+      href: siteConfig.socials.github.link,
     },
   ];
 
   return (
     <div className="flex items-center justify-center w-full">
-      <FloatingDock
-        items={links}
-        mobileClassName="translate-y-20" // only for demo, remove for production
-      />
+      <FloatingDock items={links} mobileClassName="translate-y-20" />
     </div>
   );
 }
