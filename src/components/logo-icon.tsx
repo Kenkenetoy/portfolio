02bb25d89606633036e954983d9d7786281ca528
Kenkenetoy <kennethjoseaparece@gmail.com>
2025-03-18 03:52:08 +0800

    Happy 100th commit - Contact Me Page

---
+++ src/components/logo-icon.tsx
@@ -0,0 +1,35 @@
+import { SVGProps } from "react";
+import { JSX } from "react/jsx-runtime";
+
+/* eslint-disable prettier/prettier */
+export const BrandLogoSelf = (
+  props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>
+) => (
+  <svg
+    data-name="Layer 1"
+    id="Layer_1"
+    viewBox="0 0 413.25 440.27"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <defs>
+      <style>{".cls-1{fill:#231f20}.cls-2{fill:#1c75bc}"}</style>
+    </defs>
+    <path
+      className="cls-1"
+      d="m146.64 180.31-31.22 24.34-63.61-81.58v316.92H12.23V58.68h39.58l.43.55 94.4 121.08z"
+    />
+    <path
+      className="cls-2"
+      d="m134.41 164.63-31.22 24.34-63.6-81.58v316.92H0V43h39.59l.42.55 94.4 121.08z"
+    />
+    <path
+      className="cls-1"
+      d="m182.85 226.53 166.63 213.74H299.3l-147.67-189.4-51.25 39.96-24.33-31.22 35.14-27.4 16.1-12.55 31.22-24.35 10.36-8.07 31.22-24.33L388.92 15.68l24.33 31.22-188.83 147.22-41.57 32.41z"
+    />
+    <path
+      className="cls-2"
+      d="m170.62 210.85 166.64 213.74h-50.19L139.4 235.19l-51.25 39.96-24.33-31.22 35.14-27.4 16.11-12.55 31.22-24.35 10.35-8.07 31.22-24.34L376.69 0l24.33 31.22L212.2 178.44l-41.58 32.41z"
+    />
+  </svg>
+);
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,18 +3,28 @@ import { Helmet } from "react-helmet-async";
 import { Divider } from "@heroui/divider";
 import { IconPhone } from "@tabler/icons-react";
 import { motion } from "motion/react";
+import { Button } from "@heroui/button";
+import { Input } from "@heroui/Input";
+import { Link } from "@heroui/link";
+import { Image } from "@heroui/image";
 
 import DefaultLayout from "@/layouts/default";
 import { siteConfig } from "@/config/site";
 import Footer from "@/components/footer";
-import { moveleft, moveright, moveup, ringing } from "@/anim/variants";
-import AlternatingCards from "@/components/alternating-cards";
+import {
+  cardVariants,
+  containerVariants,
+  moveleft,
+  moveright,
+  moveup,
+  ringing,
+} from "@/anim/variants";
 export default function DocsPage() {
   return (
     <>
       <Helmet>
         {/* Primary Meta Tags */}
-        <title>Blog | {siteConfig.name}</title>
+        <title>Contact | {siteConfig.name}</title>
         <meta content={siteConfig.meta_description} name="description" />
         <link href={siteConfig.url} rel="canonical" />
 
@@ -37,50 +47,179 @@ export default function DocsPage() {
       </Helmet>
 
       <DefaultLayout>
-        <div className="pt-24 mx-auto max-w-screen-2xl">
-          <motion.div
-            className="space-y-8"
-            initial="initial"
-            transition={{ duration: 0.25, ease: "circOut" }}
-            variants={moveup}
-            viewport={{ once: true, amount: 0.1 }}
-            whileInView="inView"
-          >
-            <motion.h1
-              className="flex gap-12 font-serif text-8xl"
-              initial="initial"
-              transition={{ duration: 0.75, ease: "circOut" }}
-              variants={moveright}
-              viewport={{ once: true, amount: 0.2 }}
-              whileInView="inView"
-            >
-              Let&apos;s Connect{" "}
+        <div className="pt-24 mx-auto space-y-12 max-w-screen-2xl">
+          <div className="flex">
+            <div className="min-w-[80%] space-y-12">
               <motion.div
+                className="space-y-8"
                 initial="initial"
-                transition={{ duration: 0.75, ease: "circOut" }}
-                variants={moveleft}
-                viewport={{ once: true, amount: 1 }}
+                transition={{ duration: 0.25, ease: "circOut" }}
+                variants={moveup}
+                viewport={{ once: true, amount: 0.1 }}
                 whileInView="inView"
               >
-                <motion.div className="p-2 transition-colors ease-in-out rounded-full w-fit bg-default-foreground duration-250">
+                <motion.h1
+                  className="flex gap-12 font-serif text-8xl"
+                  initial="initial"
+                  transition={{ duration: 0.75, ease: "circOut" }}
+                  variants={moveright}
+                  viewport={{ once: true, amount: 0.2 }}
+                  whileInView="inView"
+                >
+                  Let&apos;s Connect{" "}
                   <motion.div
                     initial="initial"
-                    variants={ringing}
-                    whileHover="hover"
+                    transition={{ duration: 0.75, ease: "circOut" }}
+                    variants={moveleft}
+                    viewport={{ once: true, amount: 1 }}
+                    whileInView="inView"
                   >
-                    <IconPhone
-                      className="w-20 h-20 text-default-50"
-                      stroke={1}
-                    />
+                    <motion.div className="p-2 transition-colors ease-in-out rounded-full w-fit bg-default-foreground duration-250">
+                      <motion.div
+                        initial="initial"
+                        variants={ringing}
+                        whileHover="hover"
+                      >
+                        <IconPhone
+                          className="w-20 h-20 text-default-50"
+                          stroke={1}
+                        />
+                      </motion.div>
+                    </motion.div>
+                  </motion.div>
+                </motion.h1>
+                <h2 className="w-[75%] text-2xl">
+                  Whether you&apos;re looking to collaborate on a project, need
+                  a solution to a challenging problem, or just want to talk
+                  tech, feel free to reach out. Together, we can turn ideas into
+                  reality.
+                </h2>
+              </motion.div>
+
+              <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+                viewport={{ once: true }}
+              >
+                <Divider />
+              </motion.div>
+
+              <div className="mx-auto w-[95%]">
+                <motion.div
+                  animate="show"
+                  className="flex flex-col gap-4"
+                  initial="hidden"
+                  variants={containerVariants}
+                >
+                  {["name", "email", "organization", "task", "message"].map(
+                    (field, index) => (
+                      <motion.div key={field} variants={cardVariants}>
+                        <Input
+                          isRequired
+                          className="p-3 text-default-foreground"
+                          errorMessage={`Please enter your ${field}`}
+                          label={`What’s your ${field.replace("_", " ")}?`}
+                          labelPlacement="outside"
+                          name={field}
+                          placeholder={`Enter your ${field}`}
+                          size="lg"
+                          type="text"
+                        />
+                        <motion.div
+                          initial={{ opacity: 0, y: 20 }}
+                          whileInView={{ opacity: 1, y: 0 }}
+                          transition={{ duration: 0.5, delay: index * 0.1 }}
+                          viewport={{ once: true }}
+                        >
+                          <Divider />
+                        </motion.div>
+                      </motion.div>
+                    )
+                  )}
+
+                  {/* Animated Button */}
+                  <motion.div
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.95 }}
+                    className="ml-auto"
+                  >
+                    <Button
+                      className="p-8 text-2xl"
+                      color="default"
+                      radius="full"
+                      size="lg"
+                      type="submit"
+                      variant="ghost"
+                    >
+                      Send
+                    </Button>
                   </motion.div>
                 </motion.div>
-              </motion.div>
-            </motion.h1>
+              </div>
+            </div>
 
-            <Divider />
-          </motion.div>
-          <div className="mx-auto w-[95%]">
-            <AlternatingCards />
+            <motion.div
+              className="flex flex-col items-center my-auto space-y-12 h-fit"
+              initial={{ opacity: 0, x: 50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.75, ease: "circOut" }}
+              viewport={{ once: true, amount: 1 }}
+            >
+              <Image
+                alt="Woman listening to music"
+                draggable={false}
+                radius="full"
+                src={siteConfig.logo}
+              />
+              <motion.div
+                className="space-y-12 h-fit w-fit"
+                initial={{ opacity: 0, y: 30 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.75 }}
+                viewport={{ once: true }}
+              >
+                <motion.div
+                  className="space-y-2 w-fit"
+                  initial={{ opacity: 0, y: 20 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5, delay: 0.2 }}
+                  viewport={{ once: true }}
+                >
+                  <h1>CONTACT DETAILS</h1>
+                  <p>{siteConfig.email.first}</p>
+                </motion.div>
+
+                <motion.div
+                  className="space-y-4 w-fit"
+                  initial={{ opacity: 0, y: 20 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5, delay: 0.4 }}
+                  viewport={{ once: true }}
+                >
+                  <h6 className="text-large">SOCIALS</h6>
+                  <div className="z-10 flex flex-col gap-2">
+                    {siteConfig.socials.map(({ link, icon: Icon, title }) => (
+                      <motion.div
+                        key={link}
+                        whileHover={{ scale: 1.1 }}
+                        whileTap={{ scale: 0.95 }}
+                      >
+                        <Link
+                          className="gap-2"
+                          color="foreground"
+                          href={link}
+                          target="_blank"
+                        >
+                          <Icon size={24} />
+                          <span>{title}</span>
+                        </Link>
+                      </motion.div>
+                    ))}
+                  </div>
+                </motion.div>
+              </motion.div>
+            </motion.div>
           </div>
 
           <Footer />
