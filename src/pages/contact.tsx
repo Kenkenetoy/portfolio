815a0248fb342f7bfb8427c9d154bf62216d629a
Kenkenetoy <kennethjoseaparece@gmail.com>
2025-03-22 01:37:54 +0800

    fixing contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -18,6 +18,8 @@ import Footer from "@/components/footer";
 import { moveright, moveup, ringing } from "@/anim/variants";
 
 export default function DocsPage() {
+  console.log("EmailJS Public Key:", import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
+
   const [lastSubmission, setLastSubmission] = useState<number>(0);
   const honeypotRef = useRef<HTMLInputElement>(null);
 
@@ -69,12 +71,23 @@ export default function DocsPage() {
     }
 
     try {
-      await emailjs.send(
-        import.meta.env.VITE_EMAILJS_SERVICE_ID as string,
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID as string,
-        data,
-        import.meta.env.VITE_EMAILJS_PUBLIC_KEY as string
-      );
+      emailjs
+        .send(
+          import.meta.env.VITE_EMAILJS_SERVICE_ID, // Service ID
+          import.meta.env.VITE_EMAILJS_TEMPLATE_ID, // Template ID
+          {
+            from_name: "Your Name",
+            to_name: "Receiver Name",
+            message: "Test Message",
+          },
+          import.meta.env.VITE_EMAILJS_PUBLIC_KEY // Public Key
+        )
+        .then((response) => {
+          console.log("SUCCESS!", response.status, response.text);
+        })
+        .catch((error) => {
+          console.error("FAILED...", error);
+        });
 
       addToast({
         title: "Success!",
